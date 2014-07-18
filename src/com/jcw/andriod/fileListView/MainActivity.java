04c7916209ed32b8ee44dfa2226fba80fe2d9222
Jackson Woodruff <woodruff.jackson@gmail.com>
2014-07-18 13:58:54 +0100

    added testing activity

---
+++ src/com/jcw/andriod/fileListView/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jcw.andriod.fileListView;
+
+/*
+ * Created by Jackson Woodruff on 18/07/2014 
+ *
+ * This is just a simple test case for the different
+ * types of view
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstance) {
+		super.onCreate(savedInstance);
+		setContentView(new FileOpenView(getApplicationContext()));
+	}
+}
